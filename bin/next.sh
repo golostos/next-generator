62f483378bb47833483417ea7ea197fba8cf366d
Alexander Dmitriev <alex.golostos@gmail.com>
2022-05-29 16:58:54 +0300

    First version

---
+++ bin/next.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+name=$1
+if [ ! -n "$1" ]
+then
+  read -p 'Enter the name of new app [skip for the default name: frontend]: ' project
+  if [ ! -n project ]
+  then
+    name=frontend
+  else
+    name=$project
+  fi
+fi
+
+curl https://codeload.github.com/mui/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/nextjs-with-typescript
+mv nextjs-with-typescript $name
+cd $name && npm pkg set name="$name"
+
+echo "Set version of the packages"
+arrayDeps=( $(npm pkg get dependencies | sed -n "/{/,/}/{s/\"[[:blank:]]*:[^:]*//p;}" | sed -n "s/\"//p") )
+for i in "${arrayDeps[@]}"
+do
+  version=`npm view $i version`
+	echo "$i@^$version"
+  npm pkg set dependencies.$i="^$version"
+done
+
+arrayDevDeps=( $(npm pkg get devDependencies | sed -n "/{/,/}/{s/\"[[:blank:]]*:[^:]*//p;}" | sed -n "s/\"//p") )
+for i in "${arrayDevDeps[@]}"
+do
+  version=`npm view $i version`
+	echo "$i@^$version"
+  npm pkg set devDependencies.$i="^$version"
+done
+
+cd ..
+
+FILE=package.json
+if test -f "$FILE"; then
+  npm pkg set workspaces[]="$name"
+  npm i -w $name
+else
+  cd $name && npm i
+fi
